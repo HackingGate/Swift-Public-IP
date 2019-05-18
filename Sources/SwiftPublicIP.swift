5c18d58815f8cc40438acd88c417c46cfa29817f
HackingGate <i@hackinggate.com>
2019-05-18 22:17:33 +0900

    Update description

--- Sources/SwiftPublicIP.swift
+++ Sources/SwiftPublicIP.swift
@@ -8,6 +8,13 @@
 
 import Foundation
 
+/// Get public IP address from specified API. Including icanhazip, ipv6-test...
+///
+/// - parameter url:            The API URL. Use `PublicIPAPIURLs` class. `ipv4` for IPv4 only,
+/// `ipv6` for IPv6 only, `hybrid` for both IPv4 or IPv6. You can use custom API URL but make sure
+/// it returns vilidate IP address.
+/// - parameter completion:     The result. IP address in a String.
+///
 public func getPublicIP(url: String, completion: @escaping (String?, Error?) -> Void) {
 
     guard let url: URL = URL(string: url) else {
